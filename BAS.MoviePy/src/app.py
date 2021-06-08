387cc99a1acc5ae98ffc6d345584204b00bf4fc9
kamil.kaplinski <kkaplinski97@gmail.com>
2021-06-08 18:15:39 +0200

    python prepare to deploy

--- BAS.MoviePy/src/app.py
+++ BAS.MoviePy/src/app.py
@@ -1,6 +1,8 @@
+import argparse
 from flask import Flask
 from flask_restful import Api
 from controllers.recommend.recommend import RecommendController
+from config import create_config
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,4 +10,8 @@ api = Api(app)
 api.add_resource(RecommendController, '/movies/recommend')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--environment")
+    args = parser.parse_args()
+    create_config(args.environment)
+    app.run()
--- BAS.MoviePy/src/config.py
+++ BAS.MoviePy/src/config.py
@@ -3,17 +3,27 @@ import jsons
 config = None
 
 class Config:
-    def __init__(self):
+    def __init__(self, environment):
         self.fields = None
+        self.environment = environment
         self.load_config()
 
     def load_config(self):
-        #to do: add environment variable to choose between development and release
-        config_path = 'appsettings.Development.json'
+        config_path = ''
+        if self.environment == 'Development':
+            config_path = 'appsettings.Development.json'
+        elif self.environment == 'Production':
+            config_path = 'appsettings.json'
         file_handle = open(config_path, "r")
         self.fields = jsons.loads(file_handle.read())
+        global config
+        config = self
 
 def get_config():
     if config:
         return config
     return Config()
+
+def create_config(env):
+    global config
+    config = Config(env)
